76becb328ad32e1126b0de3b5cc2324e8ff50538
nexy7574 <git@nexy7574.co.uk>
2024-08-27 16:25:07 +0100

    Fix base path

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.BASE_PATH || ""
 };
-console.log("Base path set to %s", nextConfig.basePath);
 
 export default nextConfig;
