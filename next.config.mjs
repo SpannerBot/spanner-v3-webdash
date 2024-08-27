0a0afc25a0a1e3eaf7cbf4c97067537e5bb93d6d
nexy7574 <git@nexy7574.co.uk>
2024-08-27 01:23:55 +0100

    Log the current base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
+console.log("Base path set to %s", nextConfig.basePath);
 
 export default nextConfig;
