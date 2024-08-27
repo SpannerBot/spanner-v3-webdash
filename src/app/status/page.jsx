9b2d7a086b5518c180de28687553731019dfe343
nexy7574 <git@nexy7574.co.uk>
2024-08-27 23:39:36 +0100

    Add latency information to the status page

--- src/app/status/page.jsx
+++ src/app/status/page.jsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import * as util from "../util";
 import './style.css';
 import {Avatar} from "../util";
+import {LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid} from 'recharts';
 
 function fetcher(url) {
   return util.withBackoff(async () => fetch(url), 2, 1500)
@@ -28,6 +29,37 @@ function Problems({problems, offline}) {
   )
 }
 
+function LatencyChart({history}) {
+  // History is an array of integers, each representing the latency in milliseconds.
+  // There are up to 1440 of these, to represent each minute of the day.
+  // The most recent is at the end of the array.
+
+  function arrayToData(h) {
+    let obj = {};
+    let now = new Date();
+    for(let i = 0; i < h.length; i++) {
+      const date = new Date(now - (i * 60000));
+      obj[i] = {
+        name: "Minutely latency",
+        latency: h[i],
+        date: date,
+        time: date.toLocaleTimeString()
+      };
+    }
+    return Object.values(obj);
+  }
+
+  return (
+    <LineChart width={600} height={300} data={arrayToData(history)} style={{maxWidth: "100%"}}>
+      <Line type="monotone" dataKey="latency" stroke="#5865F2"/>
+      <XAxis dataKey="time"/>
+      <YAxis/>
+      <Tooltip/>
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+
+    </LineChart>
+  )
+}
 
 export default function StatusPage() {
   const {data, error, isLoading} = useSWR(
@@ -57,18 +89,51 @@ export default function StatusPage() {
 
   let problems = [];
   let realStatus = data?.status
+  let avatarUrl=null;
+  let isOffline = !!error || !data?.online || (data?.status !== "ok");
   if(error) {
     problems.push({title: "Unable to reach the Spanner API.", description: error.message});
   }
   else {
+    if(data?.user?.avatar) {
+      avatarUrl = `https://cdn.discordapp.com/avatars/${data.user.id}/${data.user.avatar}.webp`;
+    }
     if(data?.online === false) {
       problems.push({title: "Spanner is offline.", description: "The Spanner API is currently offline."});
     }
     if(data?.guilds.unavailable.length > 0) {
       problems.push({title: "Guilds Unavailable", description: `The bot is currently unavailable in ${data.guilds.unavailable.length} guilds.`});
     }
+
+    if(data?.latency?.now > 500) {
+      problems.push(
+        {
+          title: "Increased Latency",
+          description: `The websocket latency is currently ${data.latency.now}ms. You may notice slightly ` +
+                       `slower responses.`
+        }
+      );
+    }
+    else if (data?.latency?.now > 1250) {
+      problems.push(
+        {
+          title: "High Latency",
+          description: `The websocket latency is currently ${data.latency.now}ms. You may notice slower responses.`
+        }
+      );
+    }
+    else if (data?.latency?.now > 2500) {
+      problems.push(
+        {
+          title: "Critical Latency",
+          description: `The websocket latency is currently ${data.latency.now}ms. Commands and interactions with the ` +
+                       `bot may fail..`
+        }
+      );
+      isOffline = true;
+    }
   }
-  if(!problems.length && realStatus === "ok") {
+  if(!problems.length && realStatus === "ok" && !isOffline) {
     realStatus = "nominal"
   }
 
@@ -84,13 +149,13 @@ export default function StatusPage() {
         </p>
       </div>
       <div>
-        <Problems problems={problems} offline={error || !data?.online || (data?.status !== "ok")}/>
+        <Problems problems={problems} offline={isOffline}/>
       </div>
       <div>
         <p>
           User:&nbsp;
           <Avatar
-            url={data?.user ? `https://cdn.discordapp.com/avatars/${data.user.id}/${data.user.icon}.png` : null}
+            url={avatarUrl}
             id={data?.user?.id || "4"}
           />
           {data?.user?.name || 'Unavailable'} (<code>{data?.user?.id || 'Unavailable'})</code>
@@ -102,6 +167,10 @@ export default function StatusPage() {
             </p>
           )
         }
+      </div>
+      <div>
+        <p>Current websocket latency: {data?.latency?.now || '0'} milliseconds</p>
+        {data?.latency?.history && <LatencyChart history={data.latency.history}/>}
       </div>
     </div>
   )
